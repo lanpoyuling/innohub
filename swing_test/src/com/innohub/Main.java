b65dafdbb549c153611e121a3b8720ef287c4214
lanpoyuling <1093221911@qq.com>
2017-12-06 11:59:14 +0800

    进制转换与位或运算

--- swing_test/src/com/innohub/Main.java
+++ swing_test/src/com/innohub/Main.java
@@ -8,6 +8,7 @@ public class Main {
 	/**
 	 * 物理路径拆分拼接
 	 * @param args
+	 * 
 	 */
 	public static void main(String[] args) {
 		String uploadPath="E:\\workspace\\sdny-web\\src\\main\\webapp\\upload\\";
@@ -24,6 +25,17 @@ public class Main {
 			path=path+string+"\\"+"\\";
 		}
 		System.out.println(path);
+		SysConvert();
+	}
+	
+	/**
+	 * <a href='http://blog.csdn.net/xiaochunyong/article/details/7748713'>Java 位运算(移位、位与、或、异或、非）<br>
+	 * <a href='https://jingyan.baidu.com/article/495ba84109665338b30ede98.html'>二进制、八进制、十进制、十六进制之间的转换
+	 * 
+	 */
+	public static void SysConvert(){
+		//位或运算   八进制以数字0打头   十六进制以0x打头
+		System.out.println(010 | 4);
 
 	}
 
