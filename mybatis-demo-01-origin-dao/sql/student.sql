0e3da54ad5f4f0d53252c345b3716db16c582c53
lanpoyuling <1093221911@qq.com>
2017-12-12 16:11:18 +0800

    学生表  用户表 建表语句

--- mybatis-demo-01-origin-dao/sql/student.sql
+++ mybatis-demo-01-origin-dao/sql/student.sql
@@ -7,12 +7,3 @@ CREATE TABLE student (
   stu_name varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS user;
-CREATE TABLE user (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  username varchar(32) NOT NULL COMMENT '用户名称',
-  birthday datetime DEFAULT NULL COMMENT '生日',
-  sex char(1) DEFAULT NULL COMMENT '性别',
-  address varchar(256) DEFAULT NULL COMMENT '地址',
-  PRIMARY KEY (id)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
---
+++ mybatis-demo-01-origin-dao/sql/user.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS user;
+CREATE TABLE user (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  username varchar(32) NOT NULL COMMENT '用户名称',
+  birthday datetime DEFAULT NULL COMMENT '生日',
+  sex char(1) DEFAULT NULL COMMENT '性别',
+  address varchar(256) DEFAULT NULL COMMENT '地址',
+  PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
